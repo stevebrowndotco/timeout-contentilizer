9d448b37b7ae71d00a0420e454495e4fbb12efb3
Steve Brown <contactstevebrown@gmail.com>
2014-11-07 10:51:31 +0000

    Connection test websockets

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,23 @@
 'use strict';
 
 angular
-  .module('timeoutContentilizerApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('timeoutContentilizerApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'angular-websocket'
+    ])
+    .config(function ($routeProvider, WebSocketProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+            WebSocketProvider
+                .prefix('')
+                .uri('ws://echo.websocket.org/');
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('timeoutContentilizerApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, WebSocket) {
+
+        WebSocket.onopen(function() {
+            console.log('connection');
+            WebSocket.send('message')
+        });
+
+        WebSocket.onmessage(function(event) {
+            console.log(event)
+            console.log('message: ', event.data);
+        });
+
   });
