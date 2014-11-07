e02f5b38b4cb48a88d7c2645af6332b90576195b
Steve Brown <contactstevebrown@gmail.com>
2014-11-07 10:39:01 +0000

    First commit of app with angular-websockets

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+  .module('timeoutContentilizerApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('timeoutContentilizerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
