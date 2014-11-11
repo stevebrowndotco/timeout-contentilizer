2f8f88a72d71bba91bd123d2e230ecf31f52580e
Steve Brown <contactstevebrown@gmail.com>
2014-11-11 00:29:17 +0000

    Clean up, add d3

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,7 @@ angular
     .module('realtime.main', [
         'graffiti'
     ])
-    .controller('MainCtrl', function ($scope, MessagesService, TestWebSocket, WebSocket, graffiti) {
+    .controller('MainCtrl', function ($scope, graffiti, WebSocket) {
 
         $scope.hits = {};
         $scope.paused = false;
@@ -13,13 +13,13 @@ angular
             $scope.paused = !$scope.paused;
         }
 
-        $scope.messages = MessagesService.get();
-        $scope.status = TestWebSocket.status();
         WebSocket.onmessage(function (event) {
 
             var _event = JSON.parse(event.data),
                 type = _event.uid.split('-')[1].toLowerCase() + 's',
                 id = _event.uid.split('-')[2].toLowerCase();
+
+            console.log(_event)
 
             graffiti.client('/v1/sites/'+_event.site+'/'+type+'/'+id, 'GET').then(function(data){
 
@@ -41,6 +41,7 @@ angular
             console.log('connection closed');
         });
         WebSocket.onopen(function () {
+            $scope.status = 'CONNECTED';
             console.log('connection open');
         });
 
@@ -68,107 +69,7 @@ angular
         };
 
     })
-    .factory('MessagesService', function ($q) {
-        var _messages = [
-            {
-                text: 'test message',
-                created_at: new Date()
-            }
-        ];
-        return {
-            sync: function () {
-                var dfd = $q.defer();
-                dfd.resolve(_messages)
-                return dfd.promise;
-            },
-            get: function () {
-                return _messages;
-            },
-            create: function (message) {
-                message
-            }
-        } // end return
-    })
-    .factory('TestWebSocket', function () {
-        var _status = ['DISCONNECTED', 'CONNECTED'];
-        var _currentStatus = 0;
-        var ws;
-        return {
-            status: function (url) {
-                return _status[_currentStatus];
-            },
-            new: function (wsUri) {
-                ws = new WebSocket(wsUri);
-                return ws;
-            },
-            on: function (event, callback) {
-                ws['on' + event.toLowerCase()] = callback;
-            },
-            onopen: function (callback) {
-                ws.onopen = callback;
-            },
-            onmessage: function (event) {
-                ws.onmessage
-            },
-            onclose: function () {
-            },
-            send: function () {
-            }
-        }
-    })
-var wsUri = "ws://qa17.d:3000";
-var output;
 
-function init() {
-    output = document.getElementById("output");
-    testWebSocket();
-}
-
-function onOpen(evt) {
-    writeToScreen("CONNECTED");
-    doSend("WebSocket rocks");
-}
-
-function onClose(evt) {
-    writeToScreen("DISCONNECTED");
-}
-
-function onMessage(evt) {
-    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data + '</span>');
-    websocket.close();
-}
-
-function onError(evt) {
-    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
-}
-
-function doSend(message) {
-    writeToScreen("SENT: " + message);
-    websocket.send(message);
-}
-
-function writeToScreen(message) {
-    var pre = document.createElement("p");
-    pre.style.wordWrap = "break-word";
-    pre.innerHTML = message;
-    output.appendChild(pre);
-}
-
-function testWebSocket() {
-    websocket = new WebSocket(wsUri);
-    websocket.onopen = function (evt) {
-        onOpen(evt)
-    };
-    websocket.onclose = function (evt) {
-        onClose(evt)
-    };
-    websocket.onmessage = function (evt) {
-        onMessage(evt)
-    };
-    websocket.onerror = function (evt) {
-        onError(evt)
-    };
-}
 
 var categories = ['Film', 'Music', 'Things to Do', 'Art', 'Events'];
 
